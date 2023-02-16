9841b2f18472154a713ee850406f9712dbb31d82
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-16 23:28:54 +0700

    Add Todos: Create controller TextField

--- lib/provider/todos.dart
+++ lib/provider/todos.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Todos with ChangeNotifier {}
+class Todos with ChangeNotifier {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+}
