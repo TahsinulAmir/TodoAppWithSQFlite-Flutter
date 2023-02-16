732f5ed95238a63152b3bebb18a58ec487e62614
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-16 23:32:03 +0700

    Add Todos: Call Provider Todos

--- lib/add_todo.dart
+++ lib/add_todo.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
+import 'package:todoappsqflite/provider/todos.dart';
 
 class AddTodo extends StatelessWidget {
   const AddTodo({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Todos todos = Provider.of<Todos>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Todo"),
