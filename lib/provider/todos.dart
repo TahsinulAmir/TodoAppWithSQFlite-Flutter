92fc76e44f2daa537ab988f33360892e4bbd37b7
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-16 23:43:40 +0700

    Add Todos: Create instance database in provider todos.dart & create func addTodo

--- lib/provider/todos.dart
+++ lib/provider/todos.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:todoappsqflite/db/database_manager.dart';
 
 class Todos with ChangeNotifier {
   TextEditingController titleController = TextEditingController();
   TextEditingController descController = TextEditingController();
+
+  // instansiasi database
+  DatabaseManager database = DatabaseManager.instance;
+
+  // Function addTodo
+  void addTodo() async {
+    Database db = await database.db;
+
+    await db.insert('todos', {
+      'title': titleController.text,
+      'description': descController.text,
+    });
+  }
 }
