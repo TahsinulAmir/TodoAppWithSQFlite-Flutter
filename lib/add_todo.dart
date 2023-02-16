de4ee45cf529fa87708c7e9b6fceacf473057b13
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-16 22:20:15 +0700

    Create Screen & Provider: Styling AddTodo screen (TextField & Button)

--- lib/add_todo.dart
+++ lib/add_todo.dart
@@ -11,6 +11,34 @@ class AddTodo extends StatelessWidget {
       appBar: AppBar(
         title: Text("Add Todo"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Add"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
