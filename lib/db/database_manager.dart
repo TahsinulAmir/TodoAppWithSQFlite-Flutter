9a26f33bf394873689de641f6103ef28f1b584e3
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-16 21:07:36 +0700

    Database Sqflite: database from DatabaseManager

--- lib/db/database_manager.dart
+++ lib/db/database_manager.dart
@@ -1,5 +1,9 @@
+import 'package:sqflite/sqflite.dart';
+
 class DatabaseManager {
   // privateConst
   DatabaseManager._privateConst();
   static DatabaseManager instance = DatabaseManager._privateConst();
+
+  Database? _db;
 }
